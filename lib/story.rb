837f13ae51d6176c6a4a77d947c7cf1326fbae4d
dwanerich <dwanerich@gmail.com>
2020-02-19 13:06:03 -0500

    added all class method to story file

--- lib/story.rb
+++ lib/story.rb
@@ -1,11 +1,17 @@
 class Story
 
+    @@all = []
     attr_accessor :title, :byline, :abstract
 
     def initialize(title, byline, abstract)
         @title = title
         @byline = byline
         @abstract = abstract
+        @@all << self
+    end
+
+    def self.all
+        @@all 
     end
 
 
